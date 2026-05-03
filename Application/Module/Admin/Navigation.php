89fa850aa40ee9369ee8276e4e8b90522b15e2ed
Micah <micah@aplosuite.com>
2026-05-02 20:22:08 -0600

    Resolved Navigation miss in CSS rebuild

--- Application/Module/Admin/Navigation.php
+++ Application/Module/Admin/Navigation.php
@@ -6,53 +6,53 @@
  */
 ?>
 
-<li class="app-sidebar__group-label"><span>Admin</span></li>
+<li class="side-nav__group"><span>Admin</span></li>
 
 <li>
-    <a href="/admin/userlist" class="app-sidebar__link<?= str_starts_with($currentSlug, 'admin/userlist') ? ' is-active' : '' ?>">
-        <i class="fa-solid fa-users app-sidebar__icon" aria-hidden="true"></i>
+    <a href="/admin/userlist" class="side-nav__link<?= str_starts_with($currentSlug, 'admin/userlist') ? ' is-active' : '' ?>">
+        <i class="fa-solid fa-users side-nav__icon" aria-hidden="true"></i>
         <span>Users</span>
     </a>
 </li>
 
 <li>
-    <a href="/admin/companylist" class="app-sidebar__link<?= str_starts_with($currentSlug, 'admin/companylist') ? ' is-active' : '' ?>">
-        <i class="fa-solid fa-building app-sidebar__icon" aria-hidden="true"></i>
+    <a href="/admin/companylist" class="side-nav__link<?= str_starts_with($currentSlug, 'admin/companylist') ? ' is-active' : '' ?>">
+        <i class="fa-solid fa-building side-nav__icon" aria-hidden="true"></i>
         <span>Companies</span>
     </a>
 </li>
 
 <li>
-    <a href="/admin/debug_currentuser" class="app-sidebar__link<?= str_starts_with($currentSlug, 'admin/debug_currentuser') ? ' is-active' : '' ?>">
-        <i class="fa-solid fa-bug app-sidebar__icon" aria-hidden="true"></i>
+    <a href="/admin/debug_currentuser" class="side-nav__link<?= str_starts_with($currentSlug, 'admin/debug_currentuser') ? ' is-active' : '' ?>">
+        <i class="fa-solid fa-bug side-nav__icon" aria-hidden="true"></i>
         <span>Debug: Current User</span>
     </a>
 </li>
 
 <li>
-    <a href="/admin/manageoauth" class="app-sidebar__link<?= str_starts_with($currentSlug, 'admin/manageoauth') ? ' is-active' : '' ?>">
-        <i class="fa-solid fa-key app-sidebar__icon" aria-hidden="true"></i>
+    <a href="/admin/manageoauth" class="side-nav__link<?= str_starts_with($currentSlug, 'admin/manageoauth') ? ' is-active' : '' ?>">
+        <i class="fa-solid fa-key side-nav__icon" aria-hidden="true"></i>
         <span>OAuth Sessions</span>
     </a>
 </li>
 
 <li>
-    <a href="/admin/logviewer" class="app-sidebar__link<?= str_starts_with($currentSlug, 'admin/logviewer') ? ' is-active' : '' ?>">
-        <i class="fa-solid fa-file-lines app-sidebar__icon" aria-hidden="true"></i>
+    <a href="/admin/logviewer" class="side-nav__link<?= str_starts_with($currentSlug, 'admin/logviewer') ? ' is-active' : '' ?>">
+        <i class="fa-solid fa-file-lines side-nav__icon" aria-hidden="true"></i>
         <span>System Logs</span>
     </a>
 </li>
 
 <li>
-    <a href="/admin/migrations" class="app-sidebar__link<?= str_starts_with($currentSlug, 'admin/migrations') ? ' is-active' : '' ?>">
-        <i class="fa-solid fa-database app-sidebar__icon" aria-hidden="true"></i>
+    <a href="/admin/migrations" class="side-nav__link<?= str_starts_with($currentSlug, 'admin/migrations') ? ' is-active' : '' ?>">
+        <i class="fa-solid fa-database side-nav__icon" aria-hidden="true"></i>
         <span>Migrations</span>
     </a>
 </li>
 
 <li>
-    <a href="/admin/validatortest" class="app-sidebar__link<?= str_starts_with($currentSlug, 'admin/validatortest') ? ' is-active' : '' ?>">
-        <i class="fa-solid fa-flask app-sidebar__icon" aria-hidden="true"></i>
+    <a href="/admin/validatortest" class="side-nav__link<?= str_starts_with($currentSlug, 'admin/validatortest') ? ' is-active' : '' ?>">
+        <i class="fa-solid fa-flask side-nav__icon" aria-hidden="true"></i>
         <span>Validator Tests</span>
     </a>
 </li>
--- Application/Module/CRM/Navigation.php
+++ Application/Module/CRM/Navigation.php
@@ -5,32 +5,32 @@
  */
 ?>
 
-<li class="app-sidebar__group-label"><span>CRM</span></li>
+<li class="side-nav__group"><span>CRM</span></li>
 
 <li>
-    <a href="/crm/accounts/list" class="app-sidebar__link<?= str_starts_with($currentSlug, 'crm/accounts') ? ' is-active' : '' ?>">
-        <i class="fa-solid fa-building app-sidebar__icon" aria-hidden="true"></i>
+    <a href="/crm/accounts/list" class="side-nav__link<?= str_starts_with($currentSlug, 'crm/accounts') ? ' is-active' : '' ?>">
+        <i class="fa-solid fa-building side-nav__icon" aria-hidden="true"></i>
         <span>Accounts</span>
     </a>
 </li>
 
 <li>
-    <a href="/crm/contacts/list" class="app-sidebar__link<?= str_starts_with($currentSlug, 'crm/contacts') ? ' is-active' : '' ?>">
-        <i class="fa-solid fa-address-card app-sidebar__icon" aria-hidden="true"></i>
+    <a href="/crm/contacts/list" class="side-nav__link<?= str_starts_with($currentSlug, 'crm/contacts') ? ' is-active' : '' ?>">
+        <i class="fa-solid fa-address-card side-nav__icon" aria-hidden="true"></i>
         <span>Contacts</span>
     </a>
 </li>
 
 <li>
-    <a href="/crm/opportunities/list" class="app-sidebar__link<?= str_starts_with($currentSlug, 'crm/opportunities') ? ' is-active' : '' ?>">
-        <i class="fa-solid fa-handshake app-sidebar__icon" aria-hidden="true"></i>
+    <a href="/crm/opportunities/list" class="side-nav__link<?= str_starts_with($currentSlug, 'crm/opportunities') ? ' is-active' : '' ?>">
+        <i class="fa-solid fa-handshake side-nav__icon" aria-hidden="true"></i>
         <span>Opportunities</span>
     </a>
 </li>
 
 <li>
-    <a href="/crm/products/list" class="app-sidebar__link<?= str_starts_with($currentSlug, 'crm/products') ? ' is-active' : '' ?>">
-        <i class="fa-solid fa-box app-sidebar__icon" aria-hidden="true"></i>
+    <a href="/crm/products/list" class="side-nav__link<?= str_starts_with($currentSlug, 'crm/products') ? ' is-active' : '' ?>">
+        <i class="fa-solid fa-box side-nav__icon" aria-hidden="true"></i>
         <span>Products</span>
     </a>
 </li>
